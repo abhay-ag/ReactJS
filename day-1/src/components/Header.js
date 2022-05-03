06a9db768393b33d1efc549461ea2231b7bfae1a
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-03 09:57:28 +0530

    basic header component made

---
+++ day-1/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="Header">
+        <h1>Hello, I am learning React</h1>
+    </div>
+  )
+}
+
+export default Header
