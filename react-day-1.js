8e7a344fcaccdd28da0e7688704da1a38c5c6490
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-03 09:38:11 +0530

    more about react

--- react-day-1.js
+++ react-day-1.js
@@ -1,2 +1,8 @@
 // Purely component based 
 // allows to utilize a component both on server side and client side, which boosts the speed of the development process.
+
+// flexible: easier to maintain and is flexible due the modular structure..
+// offers a virtual DOM and SSR which makes apps run fast.
+
+// usability: deploying react is fairly easy to accomplish if you have some basic knowledge of JavaScript.
+// mobile app development: use react-native for mobile development.
