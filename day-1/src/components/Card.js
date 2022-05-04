fa2586af93e4eeba2fd297adc9c8ddec7c3a22f1
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-04 11:45:53 +0530

    img size fixed

--- day-1/src/components/Card.js
+++ day-1/src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Card() {
   return (
       <div className="flex flex-col md:flex-row items-center box-border m-auto mt-5 px-4 py-2 w-fit h-fit rounded-xl bg-white">
-          <img src="/logo512.png" className='w-full md:w-20 md:h-20 box-border mx-4' alt="react-logo" />
+          <img src="/logo512.png" className='w-40 md:w-20 md:h-20 box-border mx-4' alt="react-logo" />
           <p className="flex-1 flex items-center text-2xl text-black font-bold">
             This is a react card component
           </p>
