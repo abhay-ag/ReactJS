ec4495111b1081e04d6fee6134b92034626aee85
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-05 10:47:47 +0530

    z-index bug fixed

--- react-task/src/Components/Navbar.js
+++ react-task/src/Components/Navbar.js
@@ -3,7 +3,7 @@ import '../styles/style.css'
 
 function Navbar() {
   return (
-    <div className="w-screen h-16 bg-zinc-800 flex flex-row items-center sticky top-0">
+    <div className="w-screen h-16 bg-zinc-800 flex flex-row items-center sticky top-0 z-50">
         <div className="flex-1 px-3 box-border flex flex-row font-bold text-4xl text-white items-center">Freq<font className="text-red-950">Req</font> </div>
     </div>
   )
