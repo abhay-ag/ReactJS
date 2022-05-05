74c3cdf918c16a1655a25aac7c0ff9a8855da5a0
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-05 10:44:48 +0530

    cards made | mavbar modified

--- react-task/src/App.js
+++ react-task/src/App.js
@@ -1,10 +1,33 @@
 import Navbar from './Components/Navbar';
+import Webcard from './Components/Webcard';
 import './styles/style.css'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="flex flex-col md:flex-row flex-wrap w-3/4 justify-center m-auto box-border py-14">
+        <Webcard
+          logo={<InstagramIcon />}
+          title = {"Instagram"}
+        />
+        <Webcard
+          logo={<YouTubeIcon />}
+          title = {"Youtube"}
+        />
+        <Webcard
+          logo={<GitHubIcon />}
+          title = {"Github"}
+        />
+        <Webcard
+          logo={<LinkedInIcon />}
+          title = {"LinkedIn"}
+        />
+      </div>
     </>
   );
 }
---
+++ react-task/src/Components/Webcard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/style.css'
+
+function Webcard({logo, title, desc}) {
+  return (
+    <div className="flex flex-col md:flex-row items-center box-border w-full md:w-fit h-48 md:h-fit font-bold text-black px-5 py-2 rounded-lg bg-gray-200 hover:bg-blue-200 transition ease-out my-5 md:my-5 mx-0 md:mx-5">
+        <div className="flex flex-1 mt-5 md:mt-0 items-center" style={{transform: "scale(2)"}}>
+            {logo}
+        </div>
+        <div className="text-4xl flex-1 mt-5 md:mt-0 ml-0 md:ml-5">{title}</div>
+    </div>
+  )
+}
+
+export default Webcard
