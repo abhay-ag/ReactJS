b065e0b97cd81d527e669fc570ebff0bdf9cd85f
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-05 09:43:44 +0530

    Navbar made

--- react-task/src/Components/Navbar.js
+++ react-task/src/Components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../styles/style.css'
 
 function Navbar() {
   return (
-    <div className="w-screen h-14 bg-gray-800 flex flex-row items-center font-bold text-white text-2xl">
-        
+    <div className="w-screen h-16 bg-sky-900 flex flex-row items-center">
+        <div className="flex-1 px-3 box-border flex flex-row font-bold text-4xl text-white items-center">Freq<font className="text-red-950">Req</font> </div>
     </div>
   )
 }
--- react-task/tailwind.config.js
+++ react-task/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}","./src/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        red:{
+          950: "#ff0000"
+        }
+      }
+    },
   },
   plugins: [],
 }
