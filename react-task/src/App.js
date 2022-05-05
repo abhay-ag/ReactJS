34f76e374f5289a31528d2ecc1703623335eb523
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-05 12:28:57 +0530

    webcards linked | react-router added | linking done

--- react-task/src/App.js
+++ react-task/src/App.js
@@ -1,34 +1,60 @@
 import Navbar from './Components/Navbar';
 import Webcard from './Components/Webcard';
-import './styles/style.css'
+import './styles/style.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import StackedLineChartIcon from '@mui/icons-material/StackedLineChart';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <div className="flex flex-col md:flex-row flex-wrap w-3/4 justify-center m-auto box-border py-14">
-        <Webcard
-          logo={<InstagramIcon />}
-          title = {"Instagram"}
-        />
-        <Webcard
-          logo={<YouTubeIcon />}
-          title = {"Youtube"}
-        />
-        <Webcard
-          logo={<GitHubIcon />}
-          title = {"Github"}
-        />
-        <Webcard
-          logo={<LinkedInIcon />}
-          title = {"LinkedIn"}
-        />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={
+          <>
+            <Navbar />
+            <div className="flex flex-col md:flex-row flex-wrap w-3/4 justify-center m-auto box-border py-2 md:py-14">
+              <Webcard
+                logo={<InstagramIcon />}
+                title = {"Instagram"}
+                link = {"instagram"}
+              />
+              <Webcard
+                logo={<YouTubeIcon />}
+                title = {"Youtube"}
+                link = {"youtube"}
+              />
+              <Webcard
+                logo={<GitHubIcon />}
+                title = {"Github"}
+                link = {"github"}
+              />
+              <Webcard
+                logo={<LinkedInIcon />}
+                title = {"LinkedIn"}
+                link = {"linkedin"}
+              />
+              <Webcard
+                logo={<StackedLineChartIcon />}
+                title = {"StackOverflow"}
+                link = {"stackoverflow"}
+              />
+            </div>
+          </>
+        } />
+        <Route path='/instagram' element={<Navbar />}/>
+        <Route path='/youtube' element={<Navbar />}/>
+        <Route path='/github' element={<Navbar />}/>
+        <Route path='/linkedin' element={<Navbar />}/>
+        <Route path='/stackoverflow' element={<Navbar />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- react-task/src/Components/Webcard.js
+++ react-task/src/Components/Webcard.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import '../styles/style.css'
+import { Link } from 'react-router-dom'
 
-function Webcard({logo, title, desc}) {
+function Webcard({logo, title, link}) {
   return (
-    <div className="flex flex-col md:flex-row items-center box-border w-full md:w-fit h-48 md:h-fit font-bold text-black px-5 py-2 rounded-lg bg-gray-200 hover:bg-blue-200 transition ease-out my-5 md:my-5 mx-0 md:mx-5">
-        <div className="flex flex-1 mt-5 md:mt-0 items-center" style={{transform: "scale(2)"}}>
-            {logo}
-        </div>
-        <div className="text-4xl flex-1 mt-5 md:mt-0 ml-0 md:ml-5">{title}</div>
-    </div>
+    <Link to={`/${link}`} style = {{textDecoration: "none"}}>
+      <div className="flex flex-col md:flex-row items-center box-border w-full md:w-fit h-48 md:h-fit font-bold text-black px-5 py-2 rounded-lg bg-gray-200 hover:bg-blue-200 transition ease-out my-5 md:my-5 mx-0 md:mx-5">
+          <div className="flex flex-1 mt-5 md:mt-0 items-center" style={{transform: "scale(2)"}}>
+              {logo}
+          </div>
+          <div className="text-4xl flex-1 mt-5 md:mt-0 ml-0 md:ml-5">{title}</div>
+      </div>
+    </Link>
   )
 }
 
