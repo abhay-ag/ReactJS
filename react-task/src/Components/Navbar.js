e003b31e19390a14f08cfad56d7de861d18c4c74
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-05 09:45:36 +0530

    color changed

--- react-task/src/Components/Navbar.js
+++ react-task/src/Components/Navbar.js
@@ -3,7 +3,7 @@ import '../styles/style.css'
 
 function Navbar() {
   return (
-    <div className="w-screen h-16 bg-sky-900 flex flex-row items-center">
+    <div className="w-screen h-16 bg-zinc-800 flex flex-row items-center">
         <div className="flex-1 px-3 box-border flex flex-row font-bold text-4xl text-white items-center">Freq<font className="text-red-950">Req</font> </div>
     </div>
   )
