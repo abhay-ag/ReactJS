e1d9747781a21f99357e610b50451e88be601431
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-24 09:28:55 +0530

    str.replace() implemented

--- react-day-1/src/Com1.jsx
+++ react-day-1/src/Com1.jsx
@@ -1,21 +1,24 @@
 import React, { useState } from 'react'
 
 function Com1() {
-  const [curLength, setLength] = useState(0);
-  const [flag, setFlag] = useState(false)
-  const handleCLick = () => {
-      setFlag(true)
-  }
+  const [curLength, setLength] = useState([]);
+  const [curStr, setStr] = useState("");
+  const [navStr, setNav] = useState("");
   const handleOnChange = (e) => {
-      const str = (e.target.value).split(" ")
-      setFlag(false)
-      setLength(str.length)
+      const str = e.target.value
+      if(str === ''){
+        setLength([])
+      }else{
+        setLength(str.split(" "))
+      }
+      setStr(str)
+      setNav(str.replace(/Chitkara/i, "Panjab"))
   }
   return (
     <div className='w-screen bg-red-400 h-screen flex flex-col justify-center items-center'>
-        <textarea className='w-96 h-48 rounded-xl resize-none box-border p-5 font-bold text-xl' onChange={handleOnChange}></textarea>
-        <button className='bg-blue-500 box-border mt-5 px-4 py-2 rounded font-bold text-white text-xl' onClick={handleCLick}>Display</button>
-        {flag && <p className='font-bold text-2xl'>No. of Words: {curLength}</p>}
+        <textarea value={navStr} className='w-96 h-48 rounded-xl resize-none box-border p-5 font-bold text-xl' onChange={handleOnChange}></textarea>
+        <p className='font-bold text-2xl mt-5'>No. of Words: {curLength.length}</p>
+        <p className='font-bold text-2xl mt-5'>No. of Chars: {curStr.length}</p>
     </div>
   )
 }
