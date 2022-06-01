185fd68ff900b4ea2c6881b61ef9350e493892b7
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-01 11:38:04 +0530

    Task-01 Completed || Button working fine

--- task-01/src/App.js
+++ task-01/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Stucard from './Stucard';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      student: [
+        {name: "Abhay", rNo: '0034', st1: 33, st2: 34, totM: 0},
+        {name: "Abhishek T.", rNo: '0070', st1: 32, st2: 40, totM: 0},
+        {name: "Abhishek M.", rNo: '0058', st1: 28, st2: 37, totM: 0},
+        {name: "Abhiraj", rNo: '0057', st1: 29, st2: 39, totM: 0},
+        {name: "Vaibhav", rNo: '0437', st1: 39, st2: 23, totM: 0},
+      ],
+      totalM: 0
+    }
+  }
+  total = (rNo) => {
+    let update = this.state.student.map(stu => {
+      if(rNo === stu.rNo){
+        stu.totM = stu.st1 + stu.st2;
+      }
+      return stu
+    })
+    this.setState({ student: update })
+  }
+  render() {
+    return (
+      <div className='container flex-wrap m-auto h-fit box-border py-5 bg-blue-200 flex flex-row items-center justify-center'>
+        {
+          this.state.student.map(ob => (
+            <Stucard 
+              name = {ob.name}
+              rNo = {ob.rNo}
+              m1 = {ob.st1}
+              m2 = {ob.st2}
+              totM = {ob.totM}
+              total = {this.total}
+            />
+          ))
+        }
+      </div>
+    )
+  }
 }
-
-export default App;
