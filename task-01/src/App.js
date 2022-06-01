4cd528febfaf0bb330db616a470c24a2b79baaf2
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-06-01 11:53:51 +0530

    styling done || code formatted using prettier

--- task-01/src/App.js
+++ task-01/src/App.js
@@ -1,47 +1,45 @@
-import React, { Component } from 'react'
-import Stucard from './Stucard';
-import './index.css';
+import React, { Component } from "react";
+import Stucard from "./Stucard";
+import "./index.css";
 
 export default class App extends Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
       student: [
-        {name: "Abhay", rNo: '0034', st1: 33, st2: 34, totM: 0},
-        {name: "Abhishek T.", rNo: '0070', st1: 32, st2: 40, totM: 0},
-        {name: "Abhishek M.", rNo: '0058', st1: 28, st2: 37, totM: 0},
-        {name: "Abhiraj", rNo: '0057', st1: 29, st2: 39, totM: 0},
-        {name: "Vaibhav", rNo: '0437', st1: 39, st2: 23, totM: 0},
+        { name: "Abhay", rNo: "0034", st1: 33, st2: 34, totM: 0 },
+        { name: "Abhishek T.", rNo: "0070", st1: 32, st2: 40, totM: 0 },
+        { name: "Abhishek M.", rNo: "0058", st1: 28, st2: 37, totM: 0 },
+        { name: "Abhiraj", rNo: "0057", st1: 29, st2: 39, totM: 0 },
+        { name: "Vaibhav", rNo: "0437", st1: 39, st2: 23, totM: 0 },
       ],
-      totalM: 0
-    }
+      totalM: 0,
+    };
   }
   total = (rNo) => {
-    let update = this.state.student.map(stu => {
-      if(rNo === stu.rNo){
+    let update = this.state.student.map((stu) => {
+      if (rNo === stu.rNo) {
         stu.totM = stu.st1 + stu.st2;
       }
-      return stu
-    })
-    this.setState({ student: update })
-  }
+      return stu;
+    });
+    this.setState({ student: update });
+  };
   render() {
     return (
-      <div className='container flex-wrap m-auto h-fit box-border py-5 bg-blue-200 flex flex-row items-center justify-center'>
-        {
-          this.state.student.map(ob => (
-            <Stucard 
-              name = {ob.name}
-              rNo = {ob.rNo}
-              m1 = {ob.st1}
-              m2 = {ob.st2}
-              totM = {ob.totM}
-              total = {this.total}
-            />
-          ))
-        }
+      <div className="container flex-wrap m-auto h-fit box-border py-5 bg-blue-200 flex flex-row items-center justify-center">
+        {this.state.student.map((ob) => (
+          <Stucard
+            name={ob.name}
+            rNo={ob.rNo}
+            m1={ob.st1}
+            m2={ob.st2}
+            totM={ob.totM}
+            total={this.total}
+          />
+        ))}
       </div>
-    )
+    );
   }
 }
--- task-01/src/Stucard.js
+++ task-01/src/Stucard.js
@@ -1,27 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Stucard extends Component {
-  constructor(props){
-      super(props);
-      this.state = {
-          dispTot: false,
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      dispTot: false,
+    };
   }
   showTotal = (e) => {
-      this.props.total(this.props.rNo)
-      this.setState({ dispTot: true })
-  }
+    this.props.total(this.props.rNo);
+    this.setState({ dispTot: true });
+  };
   render() {
     return (
-        <div className='w-96 h-fit flex flex-col box-border px-4 py-2 my-4 rounded-xl bg-blue-100 mx-4'>
-          <h1 className='font-bold text-3xl'>{this.props.name}</h1>
-          <h2 className='font-normal text-2xl'>{this.props.rNo}</h2>
-          <h3 className='font-medium text-xl'>Marks ST1: {this.props.m1}</h3>
-          <h3 className='font-medium text-xl'>Marks ST2: {this.props.m2}</h3>
-          <h2 className='font-bold text-2xl'>Total: {this.props.totM}</h2>
-
-          <button className='box-border px-2 py-1 rounded font-bold text-white bg-blue-500 my-3' onClick={this.showTotal}>Show Total</button>
-        </div>
-    )
+      <div className="w-96 h-fit flex flex-col box-border px-4 py-2 my-4 rounded-xl bg-blue-100 mx-4">
+        <h1 className="font-bold text-5xl">{this.props.name}</h1>
+        <h2 className="font-bold text-2xl">{this.props.rNo}</h2>
+        <h3 className="font-medium text-lg">Marks ST1: {this.props.m1}</h3>
+        <h3 className="font-medium text-lg">Marks ST2: {this.props.m2}</h3>
+        <h2 className="font-bold text-2xl flex flex-row items-center">
+          Total:
+          <div className="font-light mx-2">
+            {this.props.totM === 0 ? "Click the button" : this.props.totM}
+          </div>
+        </h2>
+        <button
+          className={`box-border px-2 py-1 rounded font-bold text-white ${
+            this.state.dispTot
+              ? "bg-blue-400 cursor-not-allowed"
+              : "bg-blue-500"
+          } my-3`}
+          disabled={this.state.dispTot}
+          onClick={this.showTotal}
+        >
+          {this.state.dispTot ? "Displayed" : "Show Total"}
+        </button>
+      </div>
+    );
   }
 }
